a1c646f091e97699cb98404d2d66e7ea894fc5ca
Gunawan050500 <gugungitaris@gmail.com>
2023-04-17 02:49:20 +0700

    progres

--- react-covid/src/components/Navbar/index.js
+++ react-covid/src/components/Navbar/index.js
@@ -1,8 +1,20 @@
+import styles from "./Navbar.module.css"
+
 function Navbar() {
   return (
-    <>
-      <h2>Example Navbar</h2>
-    </>
+    <nav className={styles.container}>
+      <div>
+        <p className={styles.brand__name}>
+          Covid ID
+        </p> 
+      </div>
+        <ul className={styles.menu__wrapper}>
+          <li className={styles.menu__item}>Global</li>
+          <li className={styles.menu__item}>Indonesia</li>
+          <li className={styles.menu__item}>Provinsi</li>
+          <li className={styles.menu__item}>About</li>
+        </ul>
+    </nav>
   );
 }
 
